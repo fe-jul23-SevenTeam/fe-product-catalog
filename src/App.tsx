6e6d621e6bc3941143de94a9495c66f9516fa739
Vitalii Serhiichuk <vitalya.sergiychuk19999@gmail.com>
2023-10-24 19:11:34 +0300

    feat: setup axios and create api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>Group Project</div>
+    <>
+      <div>Group Project</div>
+    </>
+
   );
 }
 
---
+++ src/api/axiosRequests.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const BASE_URL = 'https://be-product-catalog-fhdi.onrender.com/phones'
+
+export const getPhones = () => {
+    return axios.get(BASE_URL)
+        .then(response => {
+            return response.data;
+        })
+        .catch(error => {
+            console.error(error);
+            throw new Error(error);
+        });
+};
+
+export const getPhonesById = (phoneId: number) => {
+    return axios.get(`${BASE_URL}/${phoneId}`)
+        .then(response => {
+            return response.data;
+        })
+        .catch(error => {
+            console.error(error);
+            throw new Error(error);
+        });
+};
+
+export const getPhonesSorted = (sortField: string) => {
+    const params = {
+        sort: sortField,
+    }
+
+    return axios.get(BASE_URL, { params })
+        .then(response => {
+
+            return response.data
+        })
+        .catch(error => {
+            throw new Error(error);
+        })
+}
+
+
