e9d83de92061e419a240f2e612ef175a8685d360
Viktoriia Roik <viktoriaroik2103@gmail.com>
2023-10-25 09:18:07 +0300

    feat: create header and menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import './App.css';
+import './App.scss';
+import { Header } from './components/Header';
 
 export const App = () => {
   return (
     <>
-      <Outlet />
-
-      <div>Group Project</div>
+      {/* <Outlet /> */}
+      <Header />
+      {/* <div>Group Project</div> */}
     </>
   );
 };
