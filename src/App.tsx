b2e7d082e2fcbd3544777ac41ca9c51db979a898
Valentyn Bilyk <valentynbilyk22@gmail.com>
2023-10-25 14:15:14 +0300

    feat: create example phone card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import './App.scss';
 import { Header } from './components/Header';
+import { ProductCard } from './components/ProductCard/ProductCard';
 
 export const App = () => {
   return (
     <>
-      {/* <Outlet /> */}
       <Header />
+      <Outlet />
+      {/* <ProductCard /> */}
       {/* <div>Group Project</div> */}
     </>
   );
