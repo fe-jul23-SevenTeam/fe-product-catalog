3ab027099679b8d87ccc8d295672de347ec50130
rostyslav48 <rostyslavkravchuk05@gmail.com>
2023-10-27 15:37:06 +0300

    feat/add cart page

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -1,3 +1,26 @@
+import { BackButton } from '../../components/BackButton/BackButton';
+import { CartCard } from './components/CartCard';
+import { Checkout } from './components/Checkout';
+
+import './Cart.scss';
+
 export const CartPage = () => {
-  return <h1 className="title">CartPage</h1>;
+  return (
+    <div className="cart cart--margin-block grid wrapper">
+      <div className="cart__title-container">
+        <BackButton />
+        <h1 className="cart__title">Cart</h1>
+      </div>
+      <section className="cart__products">
+        <CartCard />
+        <CartCard />
+        <CartCard />
+        <CartCard />
+        <CartCard />
+      </section>
+      <section className="cart__checkout">
+        <Checkout />
+      </section>
+    </div>
+  );
 };
