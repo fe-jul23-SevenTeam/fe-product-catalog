90da12293df704f886faa990a06c38d67aa31186
Andrii Nakonechnyi <andreynakonechnuy11@mail.com>
2023-10-29 14:00:48 +0200

    feat: create main slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import './App.scss';
 import { Header } from './components/Header';
+import { Slider } from './Slider/Slider';
 
 export const App = () => {
   return (
     <>
-      {/* <Outlet /> */}
       <Header />
+      {/* <Outlet /> */}
+      <Slider />
       {/* <div>Group Project</div> */}
     </>
   );
