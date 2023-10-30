50314cda8fb79e37834c56fb15376a25a79c4ca6
annak1104 <137878785+annak1104@users.noreply.github.com>
2023-10-30 10:53:19 +0200

    feat: create main slider (#18)
    Co-authored-by: Andrii Nakonechnyi <andreynakonechnuy11@mail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import './App.scss';
 import { Header } from './components/Header';
+import { Slider } from './Slider/Slider';
 // import { ProductCard } from './components/ProductCard/ProductCard';
 import { Footer } from './components/Footer';
 
